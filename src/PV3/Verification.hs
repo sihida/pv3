e4dbe32f90eb0e8b442f8eae9dad8b83644b1593
Danny Bergsma <sihida@gmail.com>
2013-03-31 00:12:59 +0100

    added: check that wp is external

--- src/PV3/Verification.hs
+++ src/PV3/Verification.hs
@@ -24,18 +24,16 @@ isExternal :: Cond -> Bool
 isExternal c = External.external_Syn_Cond $ External.wrap_Cond (External.sem_Cond c) (External.Inh_Cond {})
 
 convertToSBV :: Cond -> Map String SBool -> Map String SInteger -> Symbolic SBool
-convertToSBV c m1 m2 = (Convert.symCond_Syn_Cond $ Convert.wrap_Cond (Convert.sem_Cond c) (Convert.Inh_Cond {})) m1 m2
+convertToSBV c mB mI = (Convert.symCond_Syn_Cond $ Convert.wrap_Cond (Convert.sem_Cond c) (Convert.Inh_Cond {})) mB mI
 
 verify :: Cond -> Program -> Cond -> Symbolic SBool
-verify pre program@(Program nParams _ _) post = do let vc = CImplies pre (wp program post)
-                                                   m1 <- mapM (\i -> sBool ("a" ++ show i)) [0..nParams-1]
-                                                   m2 <- mapM (\i -> sInteger ("a" ++ show i)) [0..nParams-1]
-                                                   let m1' = zip (Prelude.map (\i -> "a" ++ show i) [0..nParams-1]) m1
-                                                       m2' = zip (Prelude.map (\i -> "a" ++ show i) [0..nParams-1]) m2
-                                                       m1'' = fromList m1'
-                                                       m2'' = fromList m2'
-                                                   returnB <- sBool "return"
-                                                   returnI <- sInteger "return"
-                                                   let m1''' = insert "return" returnB m1''
-                                                       m2''' = insert "return" returnI m2''
-                                                   convertToSBV vc m1''' m2'''
+verify pre program@(Program nParams _ _) post = let vc = CImplies pre (wp program post)
+                                                in  if   isExternal vc 
+                                                    then do vB      <- mapM (\i -> sBool    ("a" ++ show i)) [0..nParams-1]
+                                                            vI      <- mapM (\i -> sInteger ("a" ++ show i)) [0..nParams-1]
+                                                            returnB <- sBool    "return"
+                                                            returnI <- sInteger "return"
+                                                            let mB = insert "return" returnB (fromList $ zip (Prelude.map (\i -> "a" ++ show i) [0..nParams-1]) vB)
+                                                                mI = insert "return" returnI (fromList $ zip (Prelude.map (\i -> "a" ++ show i) [0..nParams-1]) vI)
+                                                            convertToSBV vc mB mI
+                                                    else error "Program incorrect, wp contains references to internal (stack) data"
