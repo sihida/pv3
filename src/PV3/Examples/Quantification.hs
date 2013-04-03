86029a187f547f7007a10561b43872cd2f2d26f5
Danny Bergsma <d.bergsma@students.uu.nl>
2013-04-03 03:07:47 +0200

    added: Quantification example (working! :) )

---
+++ src/PV3/Examples/Quantification.hs
@@ -0,0 +1,26 @@
+{- |
+Example that uses (existential) quantification.
+
+Part of PV - Project 3.
+
+Authors: Jaap van der Plas and Danny Bergsma
+Version: 0.1, 3 April 2013
+-}
+
+module PV3.Examples.Quantification where
+
+import PV3.Condition.ConditionAST
+import PV3.Program.ProgramAST
+
+body :: StatementList
+body = [SInstruction (InstLoadParam 0),
+        SInstruction Instreturn]
+
+program :: Program
+program = Program 2 1 body
+                       
+postcondition :: Cond
+postcondition = CExistQuant "x" (CImplies (CAnd (CGTE (IVarName "x") (ILit 6)) (CLTE (IVarName "x") (ILit 10))) (CEQ IReturnValue (IVarName "x")))       
+
+precondition :: Cond
+precondition = CAnd (CGTE (IParamOld 0) (ILit 6)) (CLTE (IParamOld 0) (ILit 10))
