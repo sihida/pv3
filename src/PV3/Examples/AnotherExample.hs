516fc980de12eb94c0e55c26f1056bf035bad1f6
Danny Bergsma <d.bergsma@students.uu.nl>
2013-04-04 00:08:58 +0200

    minor 2

--- src/PV3/Examples/AnotherExample.hs
+++ src/PV3/Examples/AnotherExample.hs
@@ -1,35 +1,35 @@
 {- |
 Example that uses most instructions and the control structures. The program multiplies its second argument by two.
 
+int P(a0,a1) {
+  var x0 = 10;
+  var x1 = 10;
+  if (10 == 10)
+    x0 = 10;
+  else
+    x0 = 11;
+  a1 = a0 + 10;
+  if (a0 >= 10) {}
+  else 
+    return 0;
+  return a1 * 2;
+}
+
+// NB: there are a few instruction here which modify the stack, for which there is no Java-like equivalent
+
+Pre:  true
+Post: (a_0 >= 10 ==> return == (a_0 + 10) * 2) && (a_0 < 10 ==> return == 0)
+
 Part of PV - Project 3.
 
 Authors: Jaap van der Plas and Danny Bergsma
-Version: 0.1, 2 April 2013
+Version: 0.1, 3 April 2013
 -}
 module PV3.Examples.AnotherExample where
 
 import PV3.Condition.ConditionAST
 import PV3.Program.ProgramAST
 
-{- | A Java-like equivalent of this program would be:
-  
-  prog P(a0,a1) {
-    var x0 = 10;
-    var x1 = 10;
-    if (10 == 10) {
-      x0 = 10;
-    } else {
-      x0 = 11;
-    }
-    a1 = a0 + 10;
-    if (a0 >= 10) {
-    } else {
-      return 0;
-    }
-    return a1 * 2;
-  }
-  // NB: there are a few instruction here which modify the stack, for which there is no Java-like equivalent
--}
 program :: Program
 program = Program 2 2 [
   SInstruction (InstSetLocal 0 (LInt 10)),      -- local_0 := 10
@@ -61,7 +61,7 @@ program = Program 2 2 [
   SInstruction (Instreturn)]                    -- return param_1 * 2
                        
 -- | The postcondition is:
---   ((param_0 >= 10) ==> return == ((param_0 + 10) * 2)) && ((param_0 < 10) ==> return == 0)
+--   
 postcondition :: Cond
 postcondition =
   ((IParamOld 0 `CGTE` (ILit 10)) `CImplies` (IReturnValue `CEQ` ((IParamOld 0 `IAdd` (ILit 10)) `IMul` (ILit 2))))
